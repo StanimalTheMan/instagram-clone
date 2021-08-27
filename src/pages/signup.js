c329d2f4782af624022b30300fb489b2bc47eb86
StanimalTheMan <sjc591@nyu.edu>
2021-08-27 14:48:04 -0400

    attempt to setup auth, create users

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -3,10 +3,31 @@ import React from "react";
 import SEO from "../components/shared/Seo";
 import { useSignUpPageStyles } from "../styles";
 import { LoginWithFacebook } from "./login";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import { AuthContext } from "../auth";
 
 function SignUpPage() {
   const classes = useSignUpPageStyles();
+  const { signUpWithEmailAndPassword } = React.useContext(AuthContext);
+  const [values, setValues] = React.useState({
+    email: "",
+    name: "",
+    username: "",
+    password: "",
+  });
+  const history = useHistory();
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setValues((prev) => ({ ...prev, [name]: value }));
+  }
+
+  async function handleSubmit(event) {
+    console.log("wth");
+    event.preventDefault();
+    await signUpWithEmailAndPassword(values);
+    history.push("/");
+  }
 
   return (
     <>
@@ -32,8 +53,10 @@ function SignUpPage() {
               </div>
               <div className={classes.orLine} />
             </div>
-            <form>
+            <form onSubmit={handleSubmit}>
               <TextField
+                name="email"
+                onChange={handleChange}
                 fullWidth
                 variant="filled"
                 label="Email"
@@ -42,6 +65,8 @@ function SignUpPage() {
                 className={classes.textField}
               />
               <TextField
+                name="name"
+                onChange={handleChange}
                 fullWidth
                 variant="filled"
                 label="Full Name"
@@ -49,6 +74,8 @@ function SignUpPage() {
                 className={classes.textField}
               />
               <TextField
+                name="username"
+                onChange={handleChange}
                 fullWidth
                 variant="filled"
                 label="Username"
@@ -57,6 +84,8 @@ function SignUpPage() {
                 autoComplete="username"
               />
               <TextField
+                name="password"
+                onChange={handleChange}
                 fullWidth
                 variant="filled"
                 label="Password"
